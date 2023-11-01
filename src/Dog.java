eb01057c65cb489d9f15696ac2d15905c32352d2
mwild777 <123343357+mwild777@users.noreply.github.com>
2023-11-01 17:58:39 -0600

    edit of dog

--- src/Dog.java
+++ src/Dog.java
@@ -1,4 +1,20 @@
-public class Dog extends AdoptableAnimal {
+public abstract class Dog extends AdoptableAnimal {
     String breed;
 
+    public abstract String getBreed() {
+        
+        return ;
+    }
+    @Override
+    public double getPrice() {
+        return super.getPrice();
+    }
+    @Override
+    public String getDescription() {
+        return super.getDescription();
+    }
+    @Override
+    public String getName() {
+        return super.getName();
+    }
 }
